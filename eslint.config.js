c655c209be91912985a1aa32fcc301331727ede5
Daniel Lewis <daniel.d.lewis2@shell.com>
2025-05-21 20:54:47 +0100

    initial commit

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+// @ts-check
+const eslint = require('@eslint/js')
+const tseslint = require('typescript-eslint')
+const angular = require('angular-eslint')
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      'comma-dangle': ['error', 'always-multiline'],
+      'semi': ['error', 'never'],
+      'quotes': ['error', 'single', { 'avoidEscape': true }],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+)
